f338fac232faebc00d4cbfe7d1486381ad9b9669
Fernando Meira <f.lasso.h@gmail.com>
2023-08-12 22:44:09 -0300

    feat: updated storybook to 7.2.3

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,18 +1,30 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: [
     '../src/stories/**/*.stories.mdx',
     '../src/stories/**/*.stories.tsx',
   ],
+
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {}
   },
+
   features: {
     storyStoreV7: true,
   },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
