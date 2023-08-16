44c101a0ea4d721e9e1db77dfd606c061a069d89
Fernando Meira <f.lasso.h@gmail.com>
2023-08-16 00:04:42 -0300

    feat: Componente: Avatar.

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/fernando-meira.png',
+    alt: 'Fernando Meira',
+  },
+} as Meta
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
